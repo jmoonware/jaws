1c8b8ed85554897ee6765c505b58f52ec3b6431e
John Moon <moon.john.a@gmail.com>
2023-09-30 15:22:56 -0700

    Fixed multiple face detect issue, added some debugging graphics, added jawline detection and output file

--- detect_motion.py
+++ detect_motion.py
@@ -48,12 +48,15 @@ facemark_model=os.path.join(project_path,'jaws_data','lbfmodel.yaml')
 # See e.g. https://www.studytonight.com/post/dlib-68-points-face-landmark-detection-with-opencv-and-python
 POI_Upper=[51,62]
 POI_Lower=[57,66]
+POI_JAW = [7,8,9] # Points 8,9,10 span lower three jawline points
+POI_NOSE = [30,32,33,34] # top of both eyes
 
 face_cascade = cv2.CascadeClassifier(face_classifier)
 
 # list of detected faces and facemarks for each frame
 faces=[]
 facemarks=[]
+max_face=[]
 
 # create landmark detector and load lbf model:
 facemark = cv2.face.createFacemarkLBF()
@@ -63,24 +66,32 @@ def detect_and_show(frame):
 	global last_landmarks
 	global num_exceptions
 	global num_frames
+	global frame_rate
 	frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	frame_gray = cv2.equalizeHist(frame_gray)
 	# this is where we detect the face area
 	# x,y,w,h apparently returned for the face
-	faces.append(face_cascade.detectMultiScale(frame_gray))
-	if len(faces[-1]) > 0 and len(faces[-1][0])==4:
+	faces.append(face_cascade.detectMultiScale(frame_gray)) # can be multiple
+
+	# always take the largest face detected
+	face_sizes = []
+	for f in faces[-1]:
+		face_sizes.append(f[2]*f[3])
+	max_face.append(np.argmax(face_sizes))
+	if len(faces[-1]) > max_face[-1] and len(faces[-1][0])==4:
 		# upper left
-		ul=(faces[-1][0][0],faces[-1][0][1])
+		ul=(faces[-1][max_face[-1]][0],faces[-1][max_face[-1]][1])
 		# bottom right
-		br=(ul[0]+faces[-1][0][2],ul[1]+faces[-1][0][3])
-		cv2.rectangle(frame, ul, br, (0,255,0))
+		br=(ul[0]+faces[-1][max_face[-1]][2],ul[1]+faces[-1][max_face[-1]][3])
+		cv2.rectangle(frame, ul, br, (0,255,0),2)
 
 	# run landmark detector on the faces that we found (usually one):
 	num_frames+=1
 	try:
 		ok, landmarks = facemark.fit(frame_gray, faces[-1])
-		facemarks.append(landmarks)
-		last_landmarks=landmarks
+#		print(len(landmarks),max_face)
+		facemarks.append([landmarks[max_face[-1]]])
+		last_landmarks=[landmarks[max_face[-1]]]
 	except Exception as e:
 		num_exceptions+=1
 		facemarks.append(last_landmarks)
@@ -97,20 +108,37 @@ def detect_and_show(frame):
 				c=(255, 0, 0) # blue
 			if ip in POI_Lower:
 				c=(0, 0, 255) # red
-			cv2.circle(frame, (int(p[0]), int(p[1])), 1, c, 1)
+			if ip in POI_JAW or ip in POI_NOSE:
+				c=(0, 128, 255) # orange-ish
+			cv2.circle(frame, (int(p[0]), int(p[1])), 3, c, -1)
 
-	cv2.imshow('test',frame)
+	annotated_frame = cv2.putText(frame, "{0}:  {1:.2f}".format(num_frames,num_frames/frame_rate), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 
+                   1, (255,255,0), 3, cv2.LINE_AA)
+
+	cv2.imshow('output',annotated_frame)
 
 # here is where the main loop starts
 
 if not os.path.isfile(video_file):
 	sys.stderr.write("Can't open video file {0} - Goodbye\n".format(video_file))
 	sys.exit(1)
+
+cv2.namedWindow("output", cv2.WINDOW_NORMAL)
 	
 rgb=cv2.VideoCapture(video_file)	
 frame_rate=rgb.get(cv2.CAP_PROP_FPS)
 frame_count=rgb.get(cv2.CAP_PROP_FRAME_COUNT)
+f_width  = rgb.get(cv2.CAP_PROP_FRAME_WIDTH)   # float `width`
+f_height = rgb.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float `height`
+aspect = f_width/f_height
+
+resize_h = 500
+resize_w = resize_h*aspect
+
+cv2.resizeWindow("output",int(resize_w),int(resize_h)) 
+
 print("Frame Rate = {0:.4f} Hz, Frame Count = {1}, T = {2:.3f} s".format(frame_rate,frame_count,frame_count/frame_rate))
+print("Frame Width = {0:.0f} pix, Frame Height = {1:.0f}".format(f_width,f_height))
 
 if frame_end < 0:
 	frame_end=int(frame_count)
@@ -134,11 +162,14 @@ for fn in range(frame_start,frame_end):
 
 # now process the results...	
 delta_y=[]
+delta_y_jawline=[]
 idx=[]
-for im,fm,face_roi in zip(range(len(facemarks)),facemarks,faces):
+idx_jawline=[]
+print("Processing {0} facemarks, {1} faces, len max_faces {2}".format(len(facemarks),len(faces),len(max_face)))
+for im,fm,face_roi,mf in zip(range(len(facemarks)),facemarks,faces,max_face):
 	marks=fm[0][0]
-	if len(face_roi) > 0:
-		df=face_roi[0] # first and should be only detected face
+	if len(face_roi) > mf:
+		df=face_roi[mf] # largest detected face
 	else:
 		continue
 	if len(marks)==68:
@@ -148,6 +179,12 @@ for im,fm,face_roi in zip(range(len(facemarks)),facemarks,faces):
 		xu=np.mean(nm[POI_Upper][:,0])
 		xl=np.mean(nm[POI_Lower][:,0])
 
+		yu_jawline=np.mean(nm[POI_NOSE][:,1])
+		yl_jawline=np.mean(nm[POI_JAW][:,1])
+		xu_jawline=np.mean(nm[POI_NOSE][:,0])
+		xl_jawline=np.mean(nm[POI_JAW][:,0])
+
+
 		# face ROI is x,y,w,h
 		if yl>=yu and yu > df[1] and yu < df[1]+df[3] and xu > df[0] and xu < df[0]+df[2]:
 			delta_y.append(yl-yu)
@@ -155,14 +192,29 @@ for im,fm,face_roi in zip(range(len(facemarks)),facemarks,faces):
 		else:
 			print("out-of-roi index={0}".format(im))
 
+		# FIXME: make distance features an array
+		if yl_jawline>=yu_jawline and yu_jawline > df[1] and yu_jawline < df[1]+df[3] and xu_jawline > df[0] and xu_jawline < df[0]+df[2]:
+			delta_y_jawline.append(yl_jawline-yu_jawline)
+			idx_jawline.append(im)
+		else:
+			print("Jawline: out-of-roi index={0}".format(im))
+
 if show_plots:
-	plt.plot(idx,delta_y,marker='o',ms=3)
+	plt.plot(idx,delta_y,marker='o',ms=3,color='red',label='LIPS')
+	plt.plot(idx_jawline,delta_y_jawline,marker='o',ms=3,color='blue',label='JAW')
 	plt.xlabel("Frame Number")
-	plt.ylabel("Lip Distance (pixels)")
+	plt.ylabel("Distance (pixels)")
+	plt.legend()
 	plt.show()
 
 # save to file - time vs pixel distance; pixel distance gets renormalized in next step of process
 with open(output_file,'w') as f:
 	for x,y in zip(idx,delta_y):
+		f.write("{0:.6f}\t{1:.1f}\n".format((x+frame_start)/frame_rate,y))
+
+# jawline file
+jlf_name = os.path.splitext(output_file)[0]+"_jawline.txt"
+with open(jlf_name,'w') as f:
+	for x,y in zip(idx_jawline,delta_y_jawline):
 		f.write("{0:.6f}\t{1:.1f}\n".format((x+frame_start)/frame_rate,y))
 
